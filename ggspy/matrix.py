bd6b1b89082fffd7ff41c1348bf0529ead2c2311
labrunhosarodrigues <labrunhosarodrigues@gmail.com>
2022-06-02 19:27:13 +0100

    Opted for numpy cholesky

--- ggspy/matrix.py
+++ ggspy/matrix.py
@@ -14,37 +14,7 @@ Methods for Matrix manipulation.
 
 # 3rd-party
 import numpy as np
-
-# CardioID
-
-
-def cholesky(matrix):
-    """
-    Cholesky Factorization.
-
-    Parameters
-    ----------
-    matrix : n-by-n array
-        Matrix to decompose
-    
-    Returns
-    -------
-    L : n-by-n array
-        Decomposition factor L.
-    """
-
-    n = matrix.shape[0]
-    L = np.zeros(matrix.shape)
-    for j in range(n):
-        sum_ = np.sum(L[j, :j]**2)
-        L[j, j] = np.sqrt(matrix[j, j] - sum_)
-
-        for i in range(j, n):
-            sum_ = np.sum(L[i, :j]*L[j, :j])
-            L[i][j] = (matrix[i, j] - sum_)/L[j, j]
-    
-
-    return L
+import numpy.linalg as linalg
 
 
 def log_det(matrix):
@@ -63,5 +33,5 @@ def log_det(matrix):
     float
         Log-det of the matrix.
     """
-    L = cholesky(matrix)
+    L = linalg.cholesky(matrix)
     return 2*np.sum(np.log(np.diag(L)))
